c636ca098a8148d643084f0ed8a98705b2988bc2
Ankit <ankitlad1999@gmail.com>
2023-09-17 15:35:48 +0530

    uploding  files on git

---
+++ index.js
@@ -0,0 +1,41 @@
+const accessKey = "RU3skScJus61vZpRr2L-YNxaz9Z71OiOaHZkF2HCg7U";
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
+async function searchImages() {
+  keyword = searchBox.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  console.log(data);
+  const results = data.results; // Corrected variable name
+
+  results.map((result) => {
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank"; // Corrected target attribute
+
+    imageLink.appendChild(image);
+    searchResult.appendChild(imageLink);
+  });
+  showMoreBtn.style.display="block";
+}
+
+searchForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  page = 1;
+
+  await searchImages();
+});
+
+showMoreBtn.addEventListener("click",(e)=>{
+    page++;
+})
